0c772620dfca256210cb55ac8ec0e2f05443cea4
garzarobm <garzarobm@utexas.edu>
2019-09-23 11:32:24 -0500

    my files

--- PeriodicSysTickInts.c
+++ PeriodicSysTickInts.c
@@ -451,8 +451,7 @@ const uint16_t Logo[] = {
 
 
 
-#define PB0 (*((volatile uint32_t *)0x40005004))
-#define CAL 25   // cycles to service interrupt
+
 int main(void){
 	//This is for the Systick Intitialization
   PLL_Init(Bus80MHz);         // bus clock at 80 MHz
@@ -464,11 +463,6 @@ int main(void){
                               // configure PF2 as GPIO
   GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFF00FF)+0x00000000;
   GPIO_PORTF_AMSEL_R = 0;     // disable analog functionality on PF,PF3
-	
-	GPIO_PORTB_DIR_R |= 0x01;   // make PB0 output 
-  GPIO_PORTB_AFSEL_R &= ~0x01;// disable alt funct on PB0
-  GPIO_PORTB_DEN_R |= 0x01;   // enable digital I/O on PB0
-	
   SysTick_Init(8000000);        // initialize SysTick timer
   EnableInterrupts();
 //This is for the Display init
@@ -489,41 +483,37 @@ void SysTick_Handler(void){
   PF2 ^= 0x04;                // toggle PF2
   PF2 ^= 0x04;                // toggle PF2
   Counts = Counts + 1;
-	PB0 ^= 0x01;
-//	if(Counts == 2){
-//		PB0 
-//		Counts = 0;
-//	}
   PF2 ^= 0x04;                // toggle PF2
 }
 
-uint8_t Duty; // 1 to 99
-
-void Init2(void){
-  NVIC_ST_RELOAD_R = 16000*Duty-CAL;   // reload value for high
-  NVIC_ST_CTRL_R = 7;               // activate and enable interrupts
-  PB0 = 0x01;
-  EnableInterrupts();         // I = 0
-}
-void SysTick_Handler2(void){
-  if(PB0){        // end of high pulse
-    PB0 = 0x00;   // make it low
-    NVIC_ST_RELOAD_R = 16000*(100-Duty)-CAL; // reload value for low
-  }else{          // end of low pulse
-    PB0 = 0x01;   // now high
-    NVIC_ST_RELOAD_R = 16000*Duty-CAL;       // reload value for high
-  }
-}
-int main2(void){
-  PLL_Init(Bus16MHz);         // bus clock at 16 MHz
-  SYSCTL_RCGCGPIO_R |= 0x02;  // activate port B
-  Duty=25;
-  Init2();
-  GPIO_PORTB_DIR_R |= 0x01;   // make PB0 output 
-  GPIO_PORTB_AFSEL_R &= ~0x01;// disable alt funct on PB0
-  GPIO_PORTB_DEN_R |= 0x01;   // enable digital I/O on PB0
-  while(1);
-}
+//uint8_t Duty; // 1 to 99
+//#define PB0 (*((volatile uint32_t *)0x40005004))
+//#define CAL 25   // cycles to service interrupt
+//void Init2(void){
+//  NVIC_ST_RELOAD_R = 16000*Duty-CAL;   // reload value for high
+//  NVIC_ST_CTRL_R = 7;               // activate and enable interrupts
+//  PB0 = 0x01;
+//  EnableInterrupts();         // I = 0
+//}
+//void SysTick_Handler2(void){
+//  if(PB0){        // end of high pulse
+//    PB0 = 0x00;   // make it low
+//    NVIC_ST_RELOAD_R = 16000*(100-Duty)-CAL; // reload value for low
+//  }else{          // end of low pulse
+//    PB0 = 0x01;   // now high
+//    NVIC_ST_RELOAD_R = 16000*Duty-CAL;       // reload value for high
+//  }
+//}
+//int main2(void){
+//  PLL_Init(Bus16MHz);         // bus clock at 16 MHz
+//  SYSCTL_RCGCGPIO_R |= 0x02;  // activate port B
+//  Duty=25;
+//  Init2();
+//  GPIO_PORTB_DIR_R |= 0x01;   // make PB0 output 
+//  GPIO_PORTB_AFSEL_R &= ~0x01;// disable alt funct on PB0
+//  GPIO_PORTB_DEN_R |= 0x01;   // enable digital I/O on PB0
+//  while(1);
+//}
 
 
 
