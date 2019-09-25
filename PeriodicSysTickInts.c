b39e4ac759e57f791170f85f5e86d233ce98ca61
Graysless <graysonwatkins@gmail.com>
2019-09-24 20:06:30 -0700

    final

--- PeriodicSysTickInts.c
+++ PeriodicSysTickInts.c
@@ -28,10 +28,11 @@
 
 #include "../inc/SysTickInts.h"
 #include "../inc/PLL.h"
-#include "ST7735.c"
+#include "../inc/ST7735.c"
 #include "../inc/ST7735.h"
-#include "LCDDriver.c"
-#include "SwitchDriver.c"
+#include "../inc/LCDDriver.c"
+#include "../inc/SwitchDriver.c"
+#include "../inc/SoundDriver.c"
 
 //#include "SoundDriver.c"
 //#include "SwitchDriver.c"
@@ -66,9 +67,9 @@ volatile uint32_t Seconds = 0;
 volatile char* AmPm = "AM";
 volatile uint16_t Am = 1;
 volatile uint32_t AlarmHour = 12;
-volatile uint32_t	AlarmMinute = 0;
+volatile uint32_t	AlarmMinute = 1;
 volatile char*	AlarmAmPm = "AM";
-volatile uint16_t AlarmSet = 0;
+volatile uint16_t AlarmSet = 1;
 volatile uint16_t AlarmAm = 1;
 
 volatile uint32_t SpeakerCount = 0;
@@ -194,11 +195,13 @@ int main(void){
 //  GPIO_PORTB_DEN_R |= 0x01;   // enable digital I/O on PB0
 	
 	
-  SysTick_Init(80000);        // initialize SysTick timer
+  SysTick_Init(8000000);        // initialize SysTick timer
+	Sound_Init();
   EnableInterrupts();
 
 	initLCD();
 	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+	
 	
   while(1){                   // interrupts every 1ms, 500 Hz flash
     //PF3 ^= 0x08;              // toggle PF3
@@ -206,6 +209,7 @@ int main(void){
     //Switch_WaitRelease();
 		//status = Board_Input();
 		//status = Switch_Input();
+		/*
 		status = Board_Input();
     switch(status){                    // switches are negative logic on PF0 and PF4
       case 0x01: toggleAlarm(); break;   // SW1 pressed
@@ -239,6 +243,7 @@ int main(void){
 		
 		
     PF1 ^= 0x02;
+		*/
   }
 }
 
@@ -249,17 +254,10 @@ void SysTick_Handler(void){
   //PF2 ^= 0x04;                // toggle PF2
   //PF2 ^= 0x04;                // toggle PF2
   Counts = Counts + 1;
-	SpeakerCount = SpeakerCount +1;
-	if(SpeakerCount == 38){
-		PB0 ^= 0;
-		SpeakerCount = 0;
-	}
-	
-	
 	
 
-	if(Counts == 1000){
-		PF2 ^= 0x04;                // toggle PF2
+	if(Counts == 10){
+		//PF2 ^= 0x04;                // toggle PF2
 		Counts = 0;
 		Seconds++;
 	}
@@ -288,6 +286,9 @@ void SysTick_Handler(void){
 		Hours = 1;
 	}
 	
+	if((AlarmSet == 1) && (AlarmAm == Am) && (AlarmHour == Hours) && (AlarmMinute == Minutes) && (Counts == 0) && (Seconds <= 30)){
+		playAlarm();
+	}
 	
 
 	
@@ -295,6 +296,198 @@ void SysTick_Handler(void){
 }
 
 
+
+
+
+
+
+
+
+// PeriodicSysTickInts.c
+// Runs on LM4F120
+// Use the SysTick timer to request interrupts at a particular period.
+// Daniel Valvano
+// March 7, 2016
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+
+   Program 5.12, section 5.7
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// oscilloscope or LED connected to PF2 for period measurement
+
+/*
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+#include "../inc/SysTickInts.h"
+#include "../inc/PLL.h"
+#include "../inc/ST7735.c"
+#include "../inc/ST7735.h"
+#include "../inc/LCDDriver.c"
+#include "../inc/SoundDriver.c"
+#include "../inc/SwitchDriver.c"
+
+
+
+#define PF2     (*((volatile uint32_t *)0x40025010))
+#define PF3     (*((volatile uint32_t *)0x40025020))
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+volatile uint32_t Counts = 0;
+volatile uint32_t Hours = 12;
+volatile uint32_t Minutes = 0;
+volatile uint32_t Seconds = 0;
+volatile char* AmPm = "AM";
+volatile uint16_t Am = 1;
+volatile uint32_t AlarmHour = 12;
+volatile uint32_t	AlarmMinute = 0;
+volatile char*	AlarmAmPm = "AM";
+volatile uint16_t AlarmAm = 1;
+volatile uint16_t AlarmSet = 0;
+int main(void){
+	//This is for the Systick Intitialization
+  PLL_Init(Bus80MHz);         // bus clock at 80 MHz
+  SYSCTL_RCGCGPIO_R |= 0x20;  // activate port F
+  Counts = 0;
+  GPIO_PORTF_DIR_R |= 0x0C;   // make PF2,PF3 output (PF2,PF3 built-in LED)
+  GPIO_PORTF_AFSEL_R &= ~0x0C;// disable alt funct on PF2,PF3
+  GPIO_PORTF_DEN_R |= 0x0C;   // enable digital I/O on PF2,PF3
+                              // configure PF2 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFF00FF)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;     // disable analog functionality on PF,PF3
+  SysTick_Init(8000000);        // initialize SysTick timer
+  EnableInterrupts();
+
+	initLCD();
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+	
+  while(1){                   // interrupts every 1ms, 500 Hz flash
+    //PF3 ^= 0x08;              // toggle PF3
+  }
+}
+
+void incHour(void){
+	Hours++;
+	if( Hours >= 13){
+		Hours = 1;
+	}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+void incMinute(void){
+	Minutes++;
+	if(Minutes >= 60){
+		Minutes = 0;
+		Hours++;
+		if( Hours >= 13){
+			Hours = 1;
+		}
+	}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+void toggleAmPm(void){
+	if(Am == 1){
+				AmPm = "PM";
+				Am = 0;
+			}else {
+				AmPm = "AM";
+				Am = 1;
+			}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+
+void incAlarmHour(void){
+	AlarmHour++;
+	if( AlarmHour >= 13){
+		AlarmHour = 1;
+	}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+void incAlarmMinute(void){
+	AlarmMinute++;
+	if(AlarmMinute >= 60){
+		AlarmMinute = 0;
+		AlarmHour++;
+		if( AlarmHour >= 13){
+			AlarmHour = 1;
+		}
+	}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+void toggleAlarmAmPm(void){
+	if(AlarmAm == 1){
+				AlarmAmPm = "PM";
+				AlarmAm = 0;
+			}else {
+				AlarmAmPm = "AM";
+				AlarmAm = 1;
+			}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+
+void toggleAlarm(void){
+	if(AlarmSet == 0){
+		AlarmSet = 1;
+	}else {
+		AlarmSet = 0;
+	}
+	DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+}
+
+
+// Interrupt service routine
+// Executed every 12.5ns*(period)
+void SysTick_Handler(void){
+  //PF2 ^= 0x04;                // toggle PF2
+  //PF2 ^= 0x04;                // toggle PF2
+  Counts = Counts + 1;
+  //PF2 ^= 0x04;                // toggle PF2
+	if( Counts >= 10){
+		Counts = 0;
+		Seconds++;
+	}
+	if( Seconds >= 60){
+		Seconds = 0;
+		Minutes++;
+		if(Minutes < 60){
+			DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+		}
+	}
+	if(Minutes >= 60){
+		Minutes = 0;
+		Hours++;
+		if(Hours == 12){
+			if(Am == 1){
+				AmPm = "PM";
+				Am = 0;
+			}else {
+				AmPm = "AM";
+				Am = 1;
+			}
+		}
+		DrawTime(Hours, Minutes, (char*) AmPm, AlarmSet, AlarmHour, AlarmMinute, (char*) AlarmAmPm);
+	}
+	if( Hours >= 13){
+		Hours = 1;
+	}
+}
+*/
 
 
 
